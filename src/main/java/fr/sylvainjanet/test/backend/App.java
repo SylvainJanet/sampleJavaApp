c70e51402ac40f1a04b4195f895c510bb7651e29
Sylvain Janet <sjanet@jehann.fr>
2022-12-09 06:08:12 +0100

    first commit

---
+++ src/main/java/fr/sylvainjanet/test/backend/App.java
@@ -0,0 +1,32 @@
+package fr.sylvainjanet.test.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class App extends SpringBootServletInitializer {
+	@RequestMapping("/hello")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(App.class);
+	}
+
+}
