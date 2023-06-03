613e879161f5b91fa3113cbce5e9740be69bea8f
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-03 20:21:37 +0200

    Added relevant files and gitignore update

---
+++ src/test/java/fr/sylvainjanet/test/backend/TestingWebApplicationTests.java
@@ -0,0 +1,23 @@
+package fr.sylvainjanet.test.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Test the webapp.
+ *
+ * @author Sylvain
+ *
+ */
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+  /**
+   * Test if the application context can start.
+   */
+  @Test
+  public void contextLoads() {
+    // nothing to do
+  }
+
+}
